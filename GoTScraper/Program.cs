5ccebca237f3736753ce08443e7bc2c426223312
Jennifer Marsman <jennmar@microsoft.com>
2017-11-29 16:55:09 +1100

    Code cleanup - removed unused code

--- GoTScraper/Program.cs
+++ GoTScraper/Program.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
-using HtmlAgilityPack;
 using System.IO;
+using System.Net.Http;
+using HtmlAgilityPack;
 
 namespace GoTScraper
 {
@@ -51,14 +47,10 @@ namespace GoTScraper
             {
                 for (int episode = 1; episode <= 10; episode++)
                 {
-                    //Uri uri = new Uri(address + "s" + season.ToString("00") + "e" + episode.ToString("00"));
-                    //client.BaseAddress = uri;
-                    string uri = address + "s" + season.ToString("00") + "e" + episode.ToString("00");
-                    //string response = await client.GetStringAsync(uri);
-                    //ParseResponse(response);    // TODO: need to do this sync?  
-
                     // TODO: break conditions for seasons that don't have 10 episodes
+                    // TODO: why isn't it pulling season 7?  Put a check for season == 7 and episode == 1 and skip that
 
+                    string uri = address + "s" + season.ToString("00") + "e" + episode.ToString("00");
                     HtmlWeb web = new HtmlWeb();
                     HtmlDocument doc = web.Load(uri);
                     ParseResponse(doc);
@@ -68,7 +60,7 @@ namespace GoTScraper
 
         private static void ParseResponse(HtmlDocument doc)
         {
-            // Put relevant info from HTML
+            // Put relevant info from HTML - I know this is ugly and hardcoded, but it will only be run once 
             var mainContentLeftNode = doc.DocumentNode.ChildNodes["html"].ChildNodes["body"].ChildNodes[1].ChildNodes[7].ChildNodes[7].ChildNodes[3];
             string filename = mainContentLeftNode.ChildNodes["h1"].InnerText.Trim();
             string episodeTitle = mainContentLeftNode.ChildNodes["h3"].InnerText.Trim();
@@ -87,21 +79,6 @@ namespace GoTScraper
                     outputFile.WriteLine(line.Trim());
                 }
             }
-
-            /*
-            // With LINQ	
-            var nodes = doc.DocumentNode.Descendants("html")
-            .Select(y => y.Descendants()
-            .Where(x => x.Attributes["class"].Value == "main-content-left"))
-            .ToList();
-
-            // html, body, div[1], div[3], div[2]
-            // With XPath	
-            var value = doc.DocumentNode
-            .SelectNodes(@"/html[1]/body[1]")
-            .First()
-            .Attributes["value"].Value;
-            */
         }
     }
 }
